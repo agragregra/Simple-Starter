f581fb111447c469cdf96d89092540aa6b87c0a5
Алексей <agragregra@ya.ru>
2020-12-13 22:09:18 +0300

    Update

--- js/common.js
+++ js/common.js
@@ -1,7 +1,8 @@
 import * as jQuery from '../vendor/jquery/jquery-3.5.1.min.js'
+import * as magnificPopup from '../vendor/magnific-popup/jquery.magnific-popup.min.js'
 
 document.addEventListener('DOMContentLoaded', function() {
 
-	// $('body').hide()
+	$('.image-block').magnificPopup({ type: 'image' })
 
 })
