402c38971e8de97434cd72676e245528ef03387d
Алексей <agragregra@ya.ru>
2025-02-01 17:59:56 +0300

    Update

--- libs/light-dark/light-dark.js
+++ libs/light-dark/light-dark.js
@@ -1,8 +1,8 @@
 const mode = localStorage.getItem('mode') || 'auto', html = document.documentElement
 function switchMode(mode) {
 	html.style.colorScheme = mode === 'auto' ? 'light dark' : mode
-	html.classList.forEach(c => /theme$/.test(c) && html.classList.remove(c))
-	html.classList.add(mode === 'auto' ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark-theme' : 'light-theme') : mode + '-theme')
+	html.classList.forEach(c => /^theme-/.test(c) && html.classList.remove(c))
+	html.classList.add(mode === 'auto' ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'theme-dark' : 'theme-light') : 'theme-' + mode)
 	localStorage.setItem('mode', mode)
 	document.querySelectorAll('[data-mode]').forEach(el => el.classList.toggle('active', el.dataset.mode === mode))
 }
