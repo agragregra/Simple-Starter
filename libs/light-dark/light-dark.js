a29c59b920e0f9fd553793fa43df0e4dab6dcb99
Алексей <agragregra@ya.ru>
2025-02-01 22:52:11 +0300

    Update

--- libs/light-dark/light-dark.js
+++ libs/light-dark/light-dark.js
@@ -1,14 +1,27 @@
-const mode = localStorage.getItem('mode') || 'auto', html = document.documentElement
-function switchMode(mode) {
-	html.style.colorScheme = mode === 'auto' ? 'light dark' : mode
-	html.removeAttribute('data-bs-theme')
-	const newMode = mode === 'auto' ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light') : mode
-	html.classList.forEach(c => /^theme-/.test(c) && html.classList.remove(c))
-	html.classList.add(mode === 'auto' ? (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'theme-dark' : 'theme-light') : 'theme-' + mode)
+const html = document.documentElement
+const mode = localStorage.getItem('mode') || 'auto'
+
+const getPreferredMode = () => matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+
+const switchMode = mode => {
+
+	const newMode = mode === 'auto' ? getPreferredMode() : mode
+
+	html.style.colorScheme = mode === 'auto' ? 'light dark' : newMode
+	html.classList.remove(...[...html.classList].filter(c => c.startsWith('theme-')))
+	html.classList.add(`theme-${newMode}`)
 	html.setAttribute('data-bs-theme', newMode)
+
 	localStorage.setItem('mode', mode)
+
 	document.querySelectorAll('[data-mode]').forEach(el => el.classList.toggle('active', el.dataset.mode === mode))
+
 }
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => localStorage.getItem('mode') === 'auto' && switchMode('auto'))
-switchMode(mode)
+
+matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+	if (localStorage.getItem('mode') === 'auto') switchMode('auto')
+})
+
 document.addEventListener('click', e => e.target.dataset.mode && switchMode(e.target.dataset.mode))
+
+switchMode(mode)
