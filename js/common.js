f7524c2a6deddaf46e209d3e8d189b3c3650f0ca
Алексей <agragregra@ya.ru>
2025-02-01 12:21:56 +0300

    Update

--- js/common.js
+++ js/common.js
@@ -1,1 +1,13 @@
+// USER SCRIPTS
+
 // document.body.hidden = true
+
+// LIGHT DARK MODE SWITCH
+
+const mode = localStorage.getItem('mode') || 'auto', html = document.documentElement
+function switchMode(mode) {
+	html.style.colorScheme = mode === 'auto' ? 'light dark' : mode
+	localStorage.setItem('mode', mode)
+	document.querySelectorAll('[data-mode]').forEach(el => el.classList.toggle('active', el.dataset.mode === mode))
+} switchMode(mode)
+document.addEventListener('click', e => e.target.dataset.mode && switchMode(e.target.dataset.mode))
